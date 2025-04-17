8cef92566837f2c9deabb699d9b7c667ff93030d
arthur-henrique <arthur.henrique.h@outlook.com>
2025-04-17 10:07:38 -0300

    Room Manager updates
    Mudanças feitas para suportar diferentes ondas de inimigos. Também adicionada ferramenta de edição do Inspector do Room Manager para melhorar a visualização dos componentes e uso da feramenta

---
+++ Assets/Scripts/Enemies/Editor/ListComponentEditor.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.ProBuilder.Shapes;
+
+[CustomEditor(typeof(RoomManager))]
+public class ListComponentEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        // Get a reference to the script
+        RoomManager myTarget = (RoomManager)target;
+        // Draw the default inspector first (includes all public fields)
+        myTarget.isWavedRoom = EditorGUILayout.Toggle("It's a Waved Room", myTarget.isWavedRoom);
+        
+
+        // If toggle is true, show the list
+        if (myTarget.isWavedRoom)
+        {
+            SerializedProperty listProperty = serializedObject.FindProperty("enemiesWave1");
+            EditorGUILayout.PropertyField(listProperty, true); // 'true' to allow children (i.e., elements of list)
+            listProperty = serializedObject.FindProperty("enemiesWave2");
+            EditorGUILayout.PropertyField(listProperty, true); // 'true' to allow children (i.e., elements of list)
+            myTarget.isThereWave3 = EditorGUILayout.Toggle("There is a Wave 3", myTarget.isThereWave3);
+
+            if (myTarget.isThereWave3)
+            {
+                listProperty = serializedObject.FindProperty("enemiesWave3");
+                EditorGUILayout.PropertyField(listProperty, true); // 'true' to allow children (i.e., elements of list)
+            }
+        }
+        else
+        {
+            SerializedProperty listProperty = serializedObject.FindProperty("MyEnemies");
+            EditorGUILayout.PropertyField(listProperty, true); // 'true' to allow children (i.e., elements of list)
+        }
+
+        SerializedProperty doorProperty = serializedObject.FindProperty("door");
+        EditorGUILayout.PropertyField(doorProperty, true); // 'true' to allow children (i.e., elements of list)
+
+        // Apply property modifications
+        serializedObject.ApplyModifiedProperties();
+    }
+
+}
--- Assets/Scripts/Enemies/Managers/RoomManager.cs
+++ Assets/Scripts/Enemies/Managers/RoomManager.cs
@@ -5,36 +5,99 @@ using UnityEngine;
 public class RoomManager : MonoBehaviour
 {
     public List<GameObject> MyEnemies;
+    public List<GameObject> enemiesWave1;
+    public List<GameObject> enemiesWave2;
+    public List<GameObject> enemiesWave3;
+
+    public bool isWavedRoom = false;
+    public bool isThereWave3 = false;
     [SerializeField] private Door door;
 
-    private void Update()
-    {
-        if (MyEnemies.Count == 0)
-        {
-            Debug.Log("Room cleared!");
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (GameManager.instance.playerInstance == other.gameObject)
         {
-            foreach (GameObject enemy in MyEnemies)
+            if(!isWavedRoom)
             {
-                enemy.GetComponent<IdlePathfinding>().enabled = false;
-                enemy.GetComponent<EnemyMeleeCombat>().enabled = true;
+                foreach (GameObject enemy in MyEnemies)
+                {
+                    enemy.GetComponent<IdlePathfinding>().enabled = false;
+                    enemy.GetComponent<EnemyMeleeCombat>().enabled = true;
+                }
             }
+            else
+            {
+                foreach (GameObject enemy in enemiesWave1)
+                {
+                    enemy.GetComponent<IdlePathfinding>().enabled = false;
+                    enemy.GetComponent<EnemyMeleeCombat>().enabled = true;
+                }
+            }
+            
 
             Debug.Log("Player found");
         }
     }
 
+    public void SpawnSecondWave()
+    {
+        foreach (GameObject enemy in enemiesWave2)
+        {
+            enemy.SetActive(true);
+            enemy.GetComponent<IdlePathfinding>().enabled = false;
+            enemy.GetComponent<EnemyMeleeCombat>().enabled = true;
+        }
+    }
+    public void SpawnThirdWave()
+    {
+        foreach (GameObject enemy in enemiesWave3)
+        {
+            enemy.SetActive(true);
+            enemy.GetComponent<IdlePathfinding>().enabled = false;
+            enemy.GetComponent<EnemyMeleeCombat>().enabled = true;
+        }
+    }
+
     public void RemoveEnemyFromList(GameObject enemy)
     {
-        MyEnemies.Remove(enemy);
-        if (MyEnemies.Count == 0)
+        if(!isWavedRoom)
         {
-            door.OpenDoor();
+            MyEnemies.Remove(enemy);
+            if (MyEnemies.Count == 0)
+            {
+                door.OpenDoor();
+            }
+        }
+        else
+        {
+            if (enemiesWave1.Contains(enemy))
+            {
+                enemiesWave1.Remove(enemy);
+                if (enemiesWave1.Count == 0)
+                {
+                    SpawnSecondWave();
+                }
+            }
+            else if (enemiesWave2.Contains(enemy))
+            {
+                enemiesWave2.Remove(enemy);
+                if (enemiesWave2.Count == 0)
+                {
+                    if(isThereWave3)
+                        SpawnThirdWave();
+                    else
+                        door.OpenDoor();
+                }
+            }
+            else if (enemiesWave3.Contains(enemy))
+            {
+                enemiesWave3.Remove(enemy);
+                if (enemiesWave3.Count == 0)
+                {
+                    door.OpenDoor();
+                }
+            }
         }
     }
 }
