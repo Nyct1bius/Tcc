0f5ea33e74338d2d0e5a4774ede4f886aa8f3bad
arthur-henrique <63751643+arthur-henrique@users.noreply.github.com>
2025-06-06 12:20:08 -0300

    Adjustes à cena de teste

--- Assets/Scripts/Player/Combat/PlayerCombatManager.cs
+++ Assets/Scripts/Player/Combat/PlayerCombatManager.cs
@@ -187,23 +187,23 @@ public class PlayerCombatManager : MonoBehaviour
     {
         _attackIncooldown = false;
     }
-    private void OnDrawGizmos()
-    {
+    //private void OnDrawGizmos()
+    //{
 
-        Handles.color = Color.yellow;
-        Handles.DrawWireArc(transform.position, Vector3.up, Quaternion.Euler(0, -_backupWeaponData.attacks[_attackCount].attackArcAngle / 2f, 0) * transform.forward,
-            _backupWeaponData.attacks[_attackCount].attackArcAngle, _backupWeaponData.attacks[_attackCount].attackRange);
+    //    Handles.color = Color.yellow;
+    //    Handles.DrawWireArc(transform.position, Vector3.up, Quaternion.Euler(0, -_backupWeaponData.attacks[_attackCount].attackArcAngle / 2f, 0) * transform.forward,
+    //        _backupWeaponData.attacks[_attackCount].attackArcAngle, _backupWeaponData.attacks[_attackCount].attackRange);
 
-        Vector3 leftBoundary = Quaternion.Euler(0, -_backupWeaponData.attacks[_attackCount].attackArcAngle / 2f, 0) * transform.forward * _backupWeaponData.attacks[_attackCount].attackRange;
-        Vector3 rightBoundary = Quaternion.Euler(0, _backupWeaponData.attacks[_attackCount].attackArcAngle / 2f, 0) * transform.forward * _backupWeaponData.attacks[_attackCount].attackRange;
+    //    Vector3 leftBoundary = Quaternion.Euler(0, -_backupWeaponData.attacks[_attackCount].attackArcAngle / 2f, 0) * transform.forward * _backupWeaponData.attacks[_attackCount].attackRange;
+    //    Vector3 rightBoundary = Quaternion.Euler(0, _backupWeaponData.attacks[_attackCount].attackArcAngle / 2f, 0) * transform.forward * _backupWeaponData.attacks[_attackCount].attackRange;
 
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawLine(transform.position, transform.position + leftBoundary);
-        Gizmos.DrawLine(transform.position, transform.position + rightBoundary);
+    //    Gizmos.color = Color.yellow;
+    //    Gizmos.DrawLine(transform.position, transform.position + leftBoundary);
+    //    Gizmos.DrawLine(transform.position, transform.position + rightBoundary);
 
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, _lockOnRange);
-    }
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawWireSphere(transform.position, _lockOnRange);
+    //}
 
 
     private void SpawnVFXAttack()
