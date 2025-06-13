02efc5c03c0ad75253f3ae18e1e3295a342ce69f
arthur-henrique <63751643+arthur-henrique@users.noreply.github.com>
2025-06-13 10:58:21 -0300

    Daily commits

--- Assets/Scripts/Level/CameraDestroyer.cs
+++ Assets/Scripts/Level/CameraDestroyer.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class CameraDestroyer : MonoBehaviour
 {
+    
     public void DestroyAfterCutscene()
     {
         Destroy(gameObject);
+    }
+
+    public void StartGame()
+    {
+        GameManager.instance.SwitchGameState(GameManager.GameStates.Started);
     }
 }
---
+++ Assets/Scripts/Level/ElevatorLogic.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using DG.Tweening;
+public class ElevatorLogic : MonoBehaviour
+{
+    public Transform[] transforms;
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("Player"))
+    //    {
+    //        collision.transform.parent = transform;
+
+    //        if (transform.position.y != transforms[1].position.y)
+    //        {
+    //            transform.DOMoveY(transforms[1].position.y, 8f).SetEase(Ease.InOutSine).OnComplete(() => transform.DOShakePosition(0.5f, 0.25f).OnComplete(() => 
+    //            transform.position = transforms[1].position));
+    //        }
+    //        else
+    //            transform.DOMoveY(transforms[0].position.y, 8f).SetEase(Ease.InOutSine).OnComplete(() => transform.DOShakePosition(0.5f, 0.25f).OnComplete(() =>
+    //            transform.position = transforms[0].position));
+    //    }
+    //}
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    collision.transform.parent = null;
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = transform.parent;
+
+            if (transform.parent.position.y != transforms[1].position.y)
+            {
+                transform.parent.DOMoveY(transforms[1].position.y, 10f).SetEase(Ease.InOutSine).OnComplete(() => transform.parent.DOShakePosition(0.5f, 0.25f).OnComplete(() =>
+                transform.parent.position = transforms[1].position));
+            }
+            else
+                transform.parent.DOMoveY(transforms[0].position.y, 10f).SetEase(Ease.InOutSine).OnComplete(() => transform.parent.DOShakePosition(0.5f, 0.25f).OnComplete(() =>
+                transform.parent.position = transforms[0].position));
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.parent = null;
+    }
+
+}
