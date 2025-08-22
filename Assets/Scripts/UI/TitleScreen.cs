fdcc4981c1243a44a25f6fdbdca30e015620ad6d
Matheus Ferreira <89048083+matara061@users.noreply.github.com>
2025-08-22 12:07:12 -0300

    Ajustes UI titulo

--- Assets/Scripts/UI/TitleScreen.cs
+++ Assets/Scripts/UI/TitleScreen.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+using System.Collections;
+using UnityEngine.UI;
 
 public class TitleScreen : MonoBehaviour
 {
     public GameObject _fase1, _fase2;
     private LoadScene loadscene;
-
     private bool fase2 = false;
+
+    public PlayerInputs inputActions;
+
+    public GameObject botaoInicial;
 
     private void Awake()
     {
         loadscene = GetComponent<LoadScene>();
+
+        inputActions = new PlayerInputs();
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -19,6 +27,8 @@ public class TitleScreen : MonoBehaviour
     {
         _fase1.SetActive(true);
         _fase2.SetActive(false);
+
+        
     }
 
     // Update is called once per frame
@@ -33,6 +43,37 @@ public class TitleScreen : MonoBehaviour
             }
         }
     }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+       // StartCoroutine(SelecionarQuandoAtivar());
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+  /*  private IEnumerator SelecionarQuandoAtivar()
+    {
+        Debug.Log("Esperando botão ficar ativo...");
+
+        // Espera até o botão estar ativo
+        yield return new WaitUntil(() =>
+            botaoInicial != null &&
+            botaoInicial.activeInHierarchy &&
+            botaoInicial.GetComponent<Button>().interactable);
+
+        Debug.Log("Botão está ativo e interagível.");
+
+        yield return null; // espera 1 frame
+
+        EventSystem.current.SetSelectedGameObject(null); // limpa seleção atual
+        EventSystem.current.SetSelectedGameObject(botaoInicial);
+
+        Debug.Log("Botão selecionado: " + EventSystem.current.currentSelectedGameObject?.name);
+    }*/
 
     public void Continue()
     {
