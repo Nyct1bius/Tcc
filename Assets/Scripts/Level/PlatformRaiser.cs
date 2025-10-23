f49b6373f846910f51842140ea884d16c15b4c3d
arthur-henrique <63751643+arthur-henrique@users.noreply.github.com>
2025-10-23 09:55:44 -0300

    Updates to Level3

---
+++ Assets/Scripts/Level/PlatformRaiser.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class PlatformRaiser : MonoBehaviour
+{
+    public GameObject platformsParent;
+    public Transform target;
+    public float movementSpeed;
+
+
+
+    void RaisePlatforms()
+    {
+        platformsParent.transform.DOMoveY(target.position.y, movementSpeed).SetEase(Ease.InOutSine).SetUpdate(UpdateType.Fixed).OnPlay(() => transform.parent.DOShakePosition(0.5f, 0.15f));
+    }
+}
