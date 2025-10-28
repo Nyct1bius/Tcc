24dcfa15dde076879c7ab24ade9c26a24a3e02a2
Matheus Ferreira <89048083+matara061@users.noreply.github.com>
2025-10-28 09:25:39 -0300

    Music volume settings

--- Assets/Scripts/UI/AudioSettings.cs
+++ Assets/Scripts/UI/AudioSettings.cs
@@ -7,16 +7,19 @@ public class AudioSettings : MonoBehaviour
 {
 
     FMOD.Studio.Bus Master;
+    FMOD.Studio.Bus Music;
     FMOD.Studio.Bus SFX;
 
     //public float MasterVolume;
 
     [SerializeField] private Slider MasterSlider;
+    [SerializeField] private Slider MusicSlider;
     [SerializeField] private Slider SFXSlider;
 
     private void Awake()
     {
         Master = FMODUnity.RuntimeManager.GetBus("bus:/");
+        Music = FMODUnity.RuntimeManager.GetBus("bus:/Music");
         SFX = FMODUnity.RuntimeManager.GetBus("bus:/SFX");
     }
 
@@ -25,6 +28,11 @@ public class AudioSettings : MonoBehaviour
         Master.setVolume(MasterSlider.value);
     }
 
+    public void MusicVolumeLevel()
+    {
+        Music.setVolume(MusicSlider.value);
+    }
+        
     public void MasterVolumeLevel()
     {
 
