67639cdfe83c324b69aa30705d18f3f50254d25f
arthur-henrique <arthur.henrique.h@outlook.com>
2025-10-29 11:35:22 -0300

    Level End Behavior + Collider Fixes

---
+++ Assets/Scripts/Level/LevelEnd.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+    [SerializeField] private string levelToLoad;
+    [SerializeField] private LoadScene LoadScene;
+    [SerializeField] private InputReader _inputReader;
+    private bool canInteract = false;
+
+
+    private void OnEnable()
+    {
+        _inputReader.InteractEvent += Interact;
+    }
+
+    private void OnDisable()
+    {
+        _inputReader.InteractEvent -= Interact;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (GameManager.instance.PlayerInstance == other.gameObject)
+        {
+            canInteract = true;
+            print(canInteract);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (GameManager.instance.PlayerInstance == other.gameObject)
+        {
+            canInteract = false;
+            print(canInteract);
+        }
+    }
+
+    private void Interact()
+    {
+        print("HasInteracted");
+        if(canInteract)
+            LoadLevel();
+    }
+    public void LoadLevel()
+    {
+        print("HasLoadedLevel");
+        LoadScene.StartLoad(levelToLoad);
+    }
+}
